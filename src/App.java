13cbb2525fc4df88eb806d38d8aa7dec2932daaa
jordles113 <jordan.t.romero@gmail.com>
2021-02-24 16:27:55 -0600

    inital commit

---
+++ src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) {
+
+
+        float myMinFloatValue = Float.MIN_VALUE; 
+        float myMaxFloatValue = Float.MAX_VALUE; 
+
+
+        System.out.println("Float Min Value = " + myMinFloatValue); 
+        System.out.println("Float Max Valye = " + myMaxFloatValue); 
+
+
+        double myMinDoubleValue = Double.MIN_VALUE; 
+        double myMaxDoubleValue = Double.MAX_VALUE; 
+
+
+        System.out.println("Double Min Value = " + myMinDoubleValue); 
+        System.out.println("Double Max Valye = " + myMaxDoubleValue);
+    }
+}
